d9915168673339498f3c43661f0cdb6c17acbf8e
Tunnelvision77 <Tacomablues666@gmail.com>
2020-11-03 10:46:32 -0700

    more work to landing page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -19,29 +19,43 @@
         <div id="about-card" class="card">
           <div class="card-body">
             <h4 class="card-title text-center">About Me</h4>
-            <hr class="text-light" />
+            <hr class="line-break" />
             <p class="card-text">
-              Hi thanks for having me here today, It’s been a crazy year right?
-              A lot has changed in my life as well as I’m sure it has with
-              yours. One of the biggest changes in my life has been my growing
-              passion as full stack software developer with emphasis on C# and
-              JavaScript as well as the front end wire frame Vue2. As for the
-              server side I code applications in .Net, Node, MongoDb and My Sql.
-              I think I would make a great addition to your team because of my
-              knowledge and skills that I have obtained through my work career.
-              For example I helped create and run a successful Fiber Optic
-              Company where I managed customer relations as well as accounting
-              on top of the day to day work. In our first year we were able to
-              gross six figures even though we started half way through the
-              fiscal year. The next year we almost tripled that number. Through
-              my interactions with our customer Century-Link I was able to
-              double our work load to the point where the company needed to
-              expand and hire more individuals to keep up with the work flow.
-              One of my biggest goals is to expand my knowledge in application
-              development as well as bec
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
+              dignissimos velit omnis doloremque repellendus corporis, dolore
+              eum officia temporibus in at voluptates ad corrupti eius
+              reprehenderit! Placeat accusantium ad Lorem ipsum dolor sit amet,
+              consectetur adipisicing elit. Accusantium maiores ipsum tempore
+              eos necessitatibus? Itaque, qui voluptatum et quasi reprehenderit
+              quam repudiandae, quod debitis unde asperiores iure quas omnis
+              nobis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Ipsa, aut! Molestiae labore, distinctio odit dignissimos optio est
+              unde eligendi, magni voluptates voluptate tenetur voluptas nobis
+              nemo temporibus ratione deserunt consectetur. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Nihil eum consequatur nulla
+              commodi sequi. Debitis dolores odit pariatur aspernatur nostrum
+              minima eos cumque laboriosam dolore. Debitis aut omnis nostrum
+              autem!
             </p>
           </div>
         </div>
+      </article>
+      <article id="landing-skills" class="">
+        <div class="col-2 fill"></div>
+        <div class="col-8">
+          <img class="logo m-1" src="../assets/CSS.png" />
+          <img class="logo m-1" src="../assets/HTML.png" />
+          <img class="logo m-1" src="../assets/Bootstrap.png" />
+          <img class="logo m-1" src="../assets/SASS.png" />
+          <img class="logo m-1" src="../assets/VueLogo.png" />
+          <img class="logo m-1" src="../assets/JS.png" />
+          <img class="logo m-1" src="../assets/Csharp.png" />
+          <img class="logo m-1" src="../assets/NET.png" />
+          <img class="logo m-1" src="../assets/Mysql.png" />
+          <img class="logo m-1" src="../assets/Mongo.png" />
+          <img class="logo m-1" src="../assets/GIT.png" />
+        </div>
+        <div class="col-2 fill"></div>
       </article>
     </section>
   </main>
@@ -60,14 +74,26 @@ export default {
   background-size: cover;
 }
 .about-bg {
-  background-color: #3c6c80;
+  background-color: #000000;
   min-height: 70vh;
 }
 
 #about-card {
   border-radius: 50px;
-  background: #3c6c80;
-  box-shadow: 20px 20px 60px #335c6d, -20px -20px 60px #457c93;
+  background: #c03b67;
   width: 60vw;
+}
+.line-break {
+  color: white;
+}
+#landing-skills {
+  min-height: 50vh;
+  background: black;
+}
+.logo {
+  height: 12vh;
+}
+.fill {
+  min-width: 15vw;
 }
 </style>
